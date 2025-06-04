1bba8712edc40a899355d0edbecb9eef743f9415
dev8e73736 <deveshpareek001@gmail.com>
2025-06-04 15:51:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "PlexRipFilterrbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_MODE = is_enabled(environ.get('CLONE_MODE', "False"), False) # Set True or False Dev
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
